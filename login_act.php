d8ab2d00b068c365ce912f953134fe934fcdf6ff
sofiul_iam <50357469+sofiull@users.noreply.github.com>
2019-09-29 19:12:39 +0700

    Update login_act.php

--- login_act.php
+++ login_act.php
@@ -4,12 +4,12 @@ include 'admin/config.php';
 $uname=$_POST['uname'];
 $pass=$_POST['pass'];
 $pas=md5($pass);
-$query=mysqli_query($con,"select * from admin where uname='$uname' and pass='$pas'")or die(mysql_error());
+$query=mysqli_query($con,"select * from admin where usernameAdmin='$uname' and passwordAdmin='$pass'")or die(mysqli_error());
 if(mysqli_num_rows($query)==1){
 	$_SESSION['uname']=$uname;
 	header("location:admin/index.php");
 }else{
-	header("location:index.php?pesan=gagal")or die(mysql_error());
+	header("location:index.php?pesan=gagal")or die(mysqli_error());
 	// mysql_error();
 }
 // echo $pas;
