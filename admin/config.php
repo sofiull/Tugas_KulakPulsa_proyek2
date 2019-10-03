cca949df29d6a905ecb555434ab14fb4fd527cdd
Bayu Aji Pratama <50357177+path71nder@users.noreply.github.com>
2019-10-03 08:18:27 +0700

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,7 +1,7 @@
 <?php 
 $con=mysqli_connect("localhost","root","");
 mysqli_select_db($con, "kulak_pulsa");
-$operator=mysqli_query($con,"SELECT * FROM operator")or die(mysqli_error());
+$operator=mysqli_query($con,"SELECT * FROM operator")or die(mysqli_error());// mysqli_error() expects exactly 1 parameter, 0 given
 $penyedia=mysqli_query($con,"SELECT * FROM penyedia")or die(mysqli_error());
 $pulsa=mysqli_query($con,"SELECT * FROM pulsa")or die(mysqli_error());
 $nominalPulsa=mysqli_query($con,"SELECT * FROM pulsa")or die(mysqli_error());
