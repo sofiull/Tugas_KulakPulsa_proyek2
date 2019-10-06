cbd38eac7c453de4cf1732cde7002ef26693c2db
Bayu Aji Pratama <50357177+path71nder@users.noreply.github.com>
2019-10-06 08:39:30 +0700

    Update ganti_foto_act.php
    mysql to mysqli

--- admin/ganti_foto_act.php
+++ admin/ganti_foto_act.php
@@ -7,16 +7,16 @@ $foto=$_FILES['foto']['name'];
 // 	mysql_query("update admin set foto='$foto' where uname='$user'");
 
 
-$u=mysql_query("select * from admin where uname='$user'");
-$us=mysql_fetch_array($u);
+$u=mysqli_query("select * from admin where uname='$user'");
+$us=mysqli_fetch_array($u);
 if(file_exists("foto/".$us['foto'])){
 	unlink("foto/".$us['foto']);
 	move_uploaded_file($_FILES['foto']['tmp_name'], "foto/".$_FILES['foto']['name']);
-	mysql_query("update admin set foto='$foto' where uname='$user'");
+	mysqli_query("update admin set foto='$foto' where uname='$user'");
 	header("location:ganti_foto.php?pesan=oke");
 }else{
 	move_uploaded_file($_FILES['foto']['tmp_name'], "foto/".$_FILES['foto']['name']);
-	mysql_query("update admin set foto='$foto' where uname='$user'");
+	mysqli_query("update admin set foto='$foto' where uname='$user'");
 	header("location:ganti_foto.php?pesan=oke");
 }
 ?>
