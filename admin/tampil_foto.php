ccb9d7c8adfd8d5a38a2a29d6486c63daeba03f5
Bayu Aji Pratama <50357177+path71nder@users.noreply.github.com>
2019-10-24 08:17:11 +0700

    memperbaiki tampil foto

--- admin/tampil_foto.php
+++ admin/tampil_foto.php
@@ -1,6 +1,6 @@
 <?php
 	include 'config.php';
-	$sql = "SELECT * FROM users WHERE id = $id";
+	$sql = "SELECT foto FROM users WHERE id = $id";
 	$sth = $db->query($sql);
 	$result=mysqli_fetch_array($sth);
 	echo '<img src="data:image/jpeg;base64,'.base64_encode( $result['image'] ).'"/>';
