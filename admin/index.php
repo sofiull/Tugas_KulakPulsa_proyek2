94b1cfc2286b49717f828a27068c71e27bfce9e4
Bayu Aji Pratama <50357177+path71nder@users.noreply.github.com>
2019-10-28 16:17:44 +0700

    adding an "Update Now"

--- admin/index.php
+++ admin/index.php
@@ -6,15 +6,14 @@ include 'header.php';
 $con=mysqli_connect("localhost","root",""); 
 $a = mysqli_query($con,"select * from barang_laku");
 $sql= mysqli_query($con, "SELECT * FROM kulak_pulsa ORDER BY tanggal ASC LIMIT 1");
-$row= mysqli_fetch_assoc($sql);
+$row= mysqli_fetch_array($sql, MYSQLI_ASSOC);
 
-echo exec('dir');
 ?>
 
 <div class="col-md-10">
 	<h1>Selamat datang</h1>
-	<h3>Update Terakhr: </h3>	
-	<h3><?php echo $row['date']?></h3>
+	<h3>Update Terakhir: </h3>	
+	<h3><?php echo $row['tanggal']?></h3>
 </div>
 <!-- kalender -->
 <div class="pull-right">
